48527356fbcec4fbedb8e8071a1f06c8bde73f9a
ahonn <ahonn95@outlook.com>
2019-03-30 23:31:15 +0800

    fix: remove gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
         name: 'markdown-posts',
       },
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
