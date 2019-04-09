59d5063a860f11951a9dc8954cf579b036865534
ahonn <ahonn95@outlook.com>
2019-04-09 22:45:47 +0800

    chore: upgrade gatsby-source-github-issue & update .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 bash $PWD/scripts/build.sh
-if [ -e $PWD/public/index.html ]; then
-  echo 'publish /public to gh-pages branch'
-  gh-pages -d public
-else
-  exit 1
-fi
+gh-pages -d public
